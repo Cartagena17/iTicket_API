cf7f653eeb4e694a2147e0531a2f0b65882eddc2
Cartagena17 <20250040@ricaldone.edu.sv>
2026-07-12 14:06:57 -0600

    primer commit

---
+++ src/main/java/iTicket/Douglas/DouglasApplication.java
@@ -0,0 +1,23 @@
+package iTicket.Douglas;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DouglasApplication {
+
+	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
+
+		System.out.println("DB_URL cargada: " + System.getProperty("DB_URL"));
+		System.out.println("DB_USER cargada: " + System.getProperty("DB_USER"));
+
+		SpringApplication.run(DouglasApplication.class, args);
+	}
+}
